0064cc655e05870aedfa8f64e4e6dce21bc42d40
silite <32181762+silite@users.noreply.github.com>
2018-03-18 13:11:11 +0800

    None

--- TCPro/Text.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-from mywindow import mywindow
-import sys
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    windows = mywindow()
-    windows.show()
-    sys.exit(app.exec_())
