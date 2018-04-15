8d4278c1fe4d34431c7c6f6fe805d7c121b799b5
silite <32181762+silite@users.noreply.github.com>
2018-04-15 16:13:08 +0800

    None

--- 备份/重庆和值三组.py
+++ 备份/重庆和值三组.py
@@ -4,20 +4,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-user = 'silite'
-pwd = 'silite'
-# fireFoxOptions = webdriver.FirefoxOptions()
-# fireFoxOptions.set_headless()
-# driver = webdriver.Firefox(firefox_options=fireFoxOptions)
-driver = webdriver.Chrome()
+user = 'zy0108'
+pwd = 'zy731027'
+fireFoxOptions = webdriver.FirefoxOptions()
+fireFoxOptions.set_headless()
+driver = webdriver.Firefox(firefox_options=fireFoxOptions)
+#driver = webdriver.Chrome()
 driver.get('http://www.yfcp885.com/login')
 wait = WebDriverWait(driver, 10)
-TheAddList_1 = [5, 7, 9, 11, 13, 15, 17]
-TheAddList_2 = [6, 8, 10, 12, 14, 16, 18]
-TheAddList_3 = [5, 8, 9, 12, 13, 16, 17]
-TheAddList_4 = [6, 7, 10, 11, 14, 15, 18]
-TheAddList_5 = [9, 10, 11, 12, 13, 14]
-TheAddList_6 = [5, 6, 7, 8, 15, 16, 17, 18]
+TheAddList_1 = [5, 6, 7, 8, 15, 16, 17, 18]
+TheAddList_2 = [9, 10, 11, 12, 19, 20, 21, 22]
+TheAddList_3 = [5, 7, 9, 10, 12, 14, 16]
+TheAddList_4 = [6, 8, 9, 10, 11, 13, 15]
+TheAddList_5 = [3, 4, 7, 8, 11, 12, 15, 16]
+TheAddList_6 = [4, 5, 6, 9, 10, 13, 14, 17]
+TheAddList_7 = [10, 13, 14, 17, 18, 21, 22, 23]
+start_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
 qian = 1
 zhong = 2
 hou = 3
@@ -38,13 +40,18 @@ def LogIn():
         EC.url_to_be('http://www.yfcp885.com/index')
     )
 def wait_to_be_num():
-    driver.get("http://www.yfcp885.com/trendChart/1000")
     wait.until(
-        EC.presence_of_element_located((By.XPATH,'//*[@id="J-chart-content"]/tr[30]/td[5]/span')) #加载第一个
+        EC.presence_of_element_located((By.XPATH,'//*[@id="fn_getoPenGame"]/tbody[2]/tr[1]/td[2]/i')) #加载第一个
+    )
+    wait.until(
+        EC.presence_of_element_located((By.XPATH,'//*[@id="fn_getoPenGame"]/tbody[2]/tr[2]/td[2]/i')) #加载第二个
     )
 def click(x):
     time.sleep(0.3)
     driver.find_element_by_xpath(x).click()
+def PrintLog(content):
+    with open('D:\log\CqAddThreeStar_' + start_time + '.txt' ,'a') as f:
+        f.write(content)
 def GetTime():
     wait.until(
         EC.presence_of_all_elements_located((By.XPATH ,'/html/body/div/div[2]/div[1]/div[2]/em'))
@@ -54,26 +61,25 @@ def GetTime():
     Time = driver.find_element_by_xpath('/html/body/div/div[2]/div[1]/div[2]/em').text
     if len(Time) != 8:
         sys.exit()
-    sys.stdout.write('重庆当前时间' + Time + '\n')
+    sys.stdout.write('重庆和值3  :' + Time + '\n')
     sys.stdout.flush()
     if Time == '00:00:01':
         time.sleep(4)
         driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div/div[2]/span').click()
     return int(Time[-5] + Time[-4]) * 60 + int(Time[-2] + Time[-1])
-def waitTime(nowTime ,l ,h = 160):
+def waitTime(nowTime ,l ,h = 130):
     while True:
         if nowTime > l and nowTime < h:
             return nowTime
         else:
-            if nowTime == 160:
+            if nowTime == 130:
                 driver.refresh()
             nowTime = GetTime()
 def EitherBuyList(method):
-    EitherList = [0, 0, 0, 0, 0, 0]
+    EitherList = [0, 0, 0, 0, 0, 0, 0]
     wait_to_be_num()
-    WinningNum = re.compile('class="lottery-numbers">(.*?)</span>').findall(driver.page_source)
-    driver.get("http://www.yfcp885.com/lottery/SSC/1000")
-    for i in range(1, 7):
+    WinningNum = re.compile('class="numbers">(.*?)<').findall(driver.page_source)
+    for i in range(1, 8):
         BuyFlag = True
         if i == 1:
             TheAddList = TheAddList_1
@@ -87,7 +93,9 @@ def EitherBuyList(method):
             TheAddList = TheAddList_5
         elif i == 6:
             TheAddList = TheAddList_6
-        for j in range(17, 30):
+        elif i == 7:
+            TheAddList = TheAddList_7
+        for j in range(7):
             if AddNum(WinningNum[j], method) in TheAddList:
                 BuyFlag = False
                 break
@@ -110,6 +118,8 @@ def AddNum(x, method):
         return TheAdd
 def Operation(method, order):
     print("第" + str(order) + "组符合")
+    PrintLog("第" + str(order) + "组符合\n")
+    print("\a")
     method_xpath = '//*[@id="app"]/div[2]/div[2]/div[1]/div[2]/div[1]/ul[1]/li[' + str(method + 3) + ']'
     method = wait.until(
         EC.presence_of_element_located((By.XPATH, method_xpath))
@@ -124,23 +134,26 @@ def Operation(method, order):
         percent = '36'
     elif order == 2:
         TheAddList = TheAddList_2
-        percent = '31'
+        percent = '36'
     elif order == 3:
         TheAddList = TheAddList_3
         percent = '36'
     elif order == 4:
         TheAddList = TheAddList_4
-        percent = '36'
+        percent = '31'
     elif order == 5:
         TheAddList = TheAddList_5
         percent = '36'
     elif order == 6:
         TheAddList = TheAddList_6
         percent = '36'
+    elif order == 7:
+        TheAddList = TheAddList_7
+        percent = '36'
     for i in TheAddList:
         position_xpath = '//*[@id="app"]/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div/ul/li/div/div/a[' + str(i) + ']'
         click(position_xpath)
-    click('//*[@id="app"]/div[2]/div[2]/div[1]/div[2]/div[2]/div[2]/p/select/option[2]')
+    click('//*[@id="app"]/div[2]/div[2]/div[1]/div[2]/div[2]/div[2]/p/select/option[3]')
     click('//*[@id="app"]/div[2]/div[2]/div[1]/div[2]/div[2]/div[2]/a')
     click('/html/body/div/div[2]/div[2]/div[1]/div[2]/div[3]/p/label[2]')
     click('//*[@id="app"]/div[2]/div[2]/div[1]/div[2]/div[4]/div/div[1]/ul/li[3]')
@@ -171,17 +184,19 @@ def Buy(order):
     )
     money = driver.find_element_by_xpath(money_xpath).text
     if order == 1:
-        x = '640.00'
+        x = '64.80'
     elif order == 2:
-        x = '664.00'
+        x = '64.80'
     elif order == 3:
-        x = '656.00'
+        x = '64.00'
     elif order == 4:
-        x = '648.00'
+        x = '66.40'
     elif order == 5:
-        x = '656.00'
+        x = '64.00'
     elif order == 6:
-        x = '648.00'
+        x = '65.60'
+    elif order == 7:
+        x = '65.60'
     if money == x:
         click('//*[@id="app"]/div[2]/div[2]/div[1]/div[2]/div[4]/div/a')
         click('/html/body/div[2]/div[2]/div/div/div[2]/span[2]')
@@ -193,18 +208,31 @@ def Buy(order):
 def main():
     LogIn()
     while True:
-        url = 'http://www.yfcp885.com/lottery/SSC/1000'
-        driver.get(url)
-        nowTime = waitTime(GetTime(), 150)
-        if nowTime > 150:
-            for i in range(1, 4):
-                EitherList = EitherBuyList(i)
-                for j in range(1, 7):
-                    if EitherList[j - 1]:
-                        Operation(i, j)
-                        Buy(j)
-                    else:
-                        print("第" + str(j) + "组不符合")
-        time.sleep(10)
+        try:
+            url = 'http://www.yfcp885.com/lottery/SSC/1000'
+            driver.get(url)
+            nowTime = waitTime(GetTime(), 115)
+            if nowTime > 115:
+                for i in range(1, 4):
+                    if i == 1:
+                        print("----前三----")
+                        PrintLog("----前三----\n")
+                    elif i == 2:
+                        print("----中三----")
+                        PrintLog("----中三----\n")
+                    elif i == 3:
+                        print("----后三----")
+                        PrintLog("----后三----\n")
+                    EitherList = EitherBuyList(i)
+                    for j in range(1, 8):
+                        if EitherList[j - 1]:
+                            Operation(i, j)
+                            Buy(j)
+                        else:
+                            print("第" + str(j) + "组不符合")
+            PrintLog("\n")
+            time.sleep(15)
+        except Exception as e:
+            time.sleep(20)
 if __name__ == '__main__':
     main()
