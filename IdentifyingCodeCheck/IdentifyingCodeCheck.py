d24e15ee108a546c1b733b96edbe500fc862416b
silite <32181762+silite@users.noreply.github.com>
2018-05-08 10:12:16 +0800

    None

--- IdentifyingCodeCheck/IdentifyingCodeCheck.py
+++ IdentifyingCodeCheck/IdentifyingCodeCheck.py
@@ -8,7 +8,6 @@ from selenium.webdriver import ActionChains
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
 driver.get("https://www.bilibili.com")
-move_action = ActionChains(driver)
 move_btn_xpath = '//*[@id="gc-box"]/div/div[3]/div[2]'
 cropped_im_size = (650, 235)
 
@@ -20,27 +19,55 @@ def LogIn():
     move_btn = wait.until(
         EC.presence_of_element_located((By.XPATH, move_btn_xpath))
     )
-def MoveElement(distance):
-    move_action.click_and_hold(driver.find_element_by_xpath(move_btn_xpath))
-    move_action.move_by_offset(260, 0)
-    move_action.release().perform()
+def MoveElement(track):
+    ActionChains(driver).click_and_hold(driver.find_element_by_xpath(move_btn_xpath)).perform()
+    for x in track:
+        ActionChains(driver).move_by_offset(x, 0).perform()
+    time.sleep(0.5)
+    ActionChains(driver).release().perform()
 def ImageCrop():
-    move_action.click_and_hold(driver.find_element_by_xpath(move_btn_xpath))
-    move_action.move_by_offset(100, 0)
-    move_action.release().perform()
+    driver.find_element_by_xpath(move_btn_xpath).click()
     time.sleep(1)
     driver.save_screenshot('screenshot.png')
     im = Image.open('./screenshot.png')
     im_crop = im.crop((1388, 560, 2038, 795))
     im_crop.save('./cropped.png')
-    return im.getdata()
+    return im_crop.getdata()
 def GetDistance(RGBList):
+    for iter_height in range(cropped_im_size[1]):
+        for iter_width in range(cropped_im_size[0] - 110):
+            NOW_RGB = RGBList[iter_height * cropped_im_size[0] + iter_width]
+            NEXT_RGB = RGBList[iter_height * cropped_im_size[0] + iter_width + 1]
+            if NOW_RGB[0] - NEXT_RGB[0] > 60:
+                return iter_width
     return 0
+
+def GetTrack(distance):
+    v0 = 0
+    t = 0.2
+    mid_distance = distance / 2
+    now_distance = 0
+    track = []
+    while now_distance <= distance:
+        if now_distance < mid_distance:
+            a = 2
+        else:
+            a = -2
+        v = v0 + a * t
+        v0 = v
+        x = v * t + 1 / 2 * a * t * t
+        if x < 0:
+            x = 0 - x
+        now_distance += x
+        track.append(round(x))
+    return track
 def main():
     LogIn()
     RGBList = ImageCrop()
     distance = GetDistance(RGBList)
-    print(distance)
-    MoveElement(distance)
+    track = GetTrack(distance)
+    MoveElement(track)
+    time.sleep(2)
+    driver.quit()
 if __name__ == "__main__":
     main()
